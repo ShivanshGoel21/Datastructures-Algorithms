a9ddd6bdb3a1c16dae8511ab5aabf98fb751e31f
Shivansh Goel <53016763+shivanshgoel-12@users.noreply.github.com>
2020-04-24 00:15:06 +0530

    Create spliting_words_of_string.cpp

---
+++ stl/spliting_words_of_string.cpp
@@ -0,0 +1,16 @@
+// count frequency of each word in a string
+#include<iostream>
+using namespace std;
+#include<unordered_map>
+int main(){
+    string s1="geek for geeeks practise questions";
+    string word="";
+    for(auto x: s1){
+        if(x==' '){
+            cout<<word<<endl;
+            word="";
+        }
+        else word=word+x;
+    }
+    return 0;
+}
