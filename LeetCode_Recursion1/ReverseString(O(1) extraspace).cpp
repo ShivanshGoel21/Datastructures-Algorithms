50203e9cdc6da97f501014a93b626e3b5bed92bc
Shivansh Goel <53016763+shivanshgoel-12@users.noreply.github.com>
2020-05-18 13:35:33 +0530

    Create ReverseString(O(1) extraspace).cpp

---
+++ LeetCode_Recursion1/ReverseString(O(1) extraspace).cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+	void reverse(vector<char>& s,int i,int j){
+        if(i>=j) return;
+        swap(s[i],s[j]);
+        reverse(s,++i,--j);
+        
+}
+    void reverseString(vector<char>& s) {
+        reverse(s,0,s.size()-1);
+    }
+};
